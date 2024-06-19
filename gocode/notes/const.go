be0460b12676339e64eac586f1ac154af0c9b4d0
Manish Sharma <ms2641188@gmail.com>
2024-06-19 11:42:25 +0530

    feat: Update the modes info

---
+++ gocode/notes/const.go
@@ -0,0 +1,16 @@
+package main
+
+const (
+	normalModeDesc      = "for normal text notes (Applicable for all OPTION) [DEFAULT IN CASE MODE NOT PROVIDED]"
+	commandModeDesc     = "for executable commands (Applicable for all OPTION)"
+	allModesDesc        = "for all text notes and executable commands (Applicable for only Get, Delete OPTION)"
+	createOptionDesc    = "Create a note/command"
+	updateOptionDesc    = "Update a note/command"
+	listOptionDesc      = "List all notes/commands"
+	listInfoOptionDesc  = "List all notes/commands with info"
+	getOptionDesc       = "Get info of a note/command"
+	deleteOptionDesc    = "Delete a note/command"
+	deleteAllOptionDesc = "Delete all notes/commands"
+	helpOptionDesc      = "Display this help message"
+	newLine             = "\n"
+)
